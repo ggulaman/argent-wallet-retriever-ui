50a9e90a8b2b369d574672dad2b0cd269ea4a447
ggulaman <gulamman@protonmail.com>
2022-11-12 22:29:58 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Home from "./pages/Home";
+import { orange, blue } from "@mui/material/colors";
 
-function App() {
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: orange[500],
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: blue[500],
+      contrastText: "#fff",
+    },
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box>
+        <Home></Home>
+      </Box>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
